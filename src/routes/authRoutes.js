247baf24c21ee2a37eb3fccd0f7a39beb8eeac20
dolwinf <dolwinf@gmail.com>
2023-09-30 12:41:19 +1000

    cleanup code

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,9 +2,6 @@ const express = require("express");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const User = require("../models/UserModel");
-const validator = require("validator");
-const nodemailer = require("nodemailer");
-const crypto = require("crypto");
 const authMiddleware = require("../middleware/authMiddleware");
 const dotenv = require("dotenv");
 dotenv.config();
@@ -31,7 +28,6 @@ passport.use(
       }
 
       // If not, create a new user
-      //Need to fix this part to save a user to the DB
       const user = await new User({
         email: email,
         first_name: given_name,
@@ -67,98 +63,5 @@ router.get(
     res.redirect("/");
   }
 );
-
-router.post("/register", async (req, res) => {
-  const { email, password } = req.body;
-
-  if (email === null || email === undefined || !validator.isEmail(email)) {
-    return res.status(400).json({ message: "Invalid email" });
-  }
-  if (password.length <= 8 || password === null || password === undefined) {
-    return res
-      .status(400)
-      .json({ message: "Password should be more than 8 characters" });
-  }
-
-  // Check if user exists
-  const user = await User.findOne({ email: req.body.email });
-  if (user) {
-    return res
-      .status(400)
-      .json({ message: "Email has already been registered" });
-  }
-
-  // Create a validation token
-  const validationToken = crypto.randomBytes(20).toString("hex");
-
-  //Send an email to the user with the validation token
-  // Send validation email first
-  const mailOptions = {
-    from: "dolwinf@gmail.com",
-    to: email,
-    subject: "Email Validation",
-    text: `Please validate your email by clicking on the following link: http://localhost:3000/validate-account?token=${validationToken}`,
-  };
-
-  //User will click on the link, that should go to an endpoint that fetches the email and retrieves the token and compares it and then marks the account
-  //as activated if the token matches.
-
-  try {
-    await transporter.sendMail(mailOptions);
-  } catch (e) {
-    console.log(e);
-  }
-
-  try {
-    const user = new User(req.body);
-    user.validation_token = validationToken;
-    user.account_validated = false;
-    await user.save();
-    res.status(201).send({
-      message:
-        "Please check your email and click on the validation link to activate your account",
-    });
-  } catch (error) {
-    res.status(400).send(error);
-  }
-});
-
-router.get("/validate-account", async (req, res) => {
-  const { token } = req.query;
-
-  // Retrieve the user associated with this token
-  const user = await User.findOne({ validation_token: token });
-
-  if (!user) {
-    return res
-      .status(400)
-      .json({ message: "Invalid or expired validation token" });
-  }
-
-  user.validation_token = null;
-
-  try {
-    await user.save();
-    res.status(200).json({ message: "Email validated successfully" });
-  } catch (error) {
-    res.status(500).json({ message: "Error validating email" });
-  }
-});
-
-router.post("/login", async (req, res) => {
-  try {
-    const user = await User.findOne({ email: req.body.email });
-    if (!user || !(await bcrypt.compare(req.body.password, user.password))) {
-      return res.status(400).send({ message: "Invalid email or password" });
-    }
-
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-      expiresIn: "1h",
-    });
-    res.status(200).send({ token });
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
 
 module.exports = router;
